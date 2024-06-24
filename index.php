a630e3309b68d9dd46a9d29bb4ebe45a4194168d
abhayv2024 <abhayverma7389@gmail.com>
2024-06-24 13:27:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+        <title>login</title>
+        <style>
+            #a{
+                border: 1px solid black;
+                height: 400px;
+                width: 350px;
+                margin-left: 500px;
+                margin-top: 100px;
+                background-image: url(ll.jfif);
+                background-repeat: no-repeat;
+                background-size: cover;
+
+            }
+            table{
+                line-height: 40px;
+                margin-top: 50px;
+                font-size: 22px;
+                margin-left: 10px;
+                
+
+
+            }
+            .a1{
+                height: 28px;
+                width: 300px;
+            }
+            #a2{
+                height: 36px;
+                width: 130px;
+                margin-top: 30px;
+                margin-left: 90px;
+                  
+            }
+            a{
+                margin-left: 180px;
+                margin-top: 30px;
+            }
+            h2{
+                margin-left: 110px;
+                font-size: 31px;
+    
+            }
+            body{
+                background-image: url(log.jfif);
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <html>
+        <form>
+            <div id="a">
+            <table>
+                <h2>ADMIN</h2>
+                <tr><td>EMAIL / MOBILENO. :</td></tr>
+                <tr><td><input type="text" class="a1"></td></tr>
+                <tr><td>PASSWORD:</td></tr>
+                <tr><td><input type="text" class="a1"></td></tr>
+                <tr><td><input type="submit" value="LOGIN" id="a2"></td></tr>
+                <tr><td><a href="signup.html">NEW USER ?</a></td></tr>
+            </table>
+            </div>
+        </form>
+    </html>
+</html>
